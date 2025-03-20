d2a94b1685b642a9dca254306b0e0d847eaa81d2
Juha Laurikainen <jmlaurikainen@gmail.com>
2025-03-20 13:20:52 +0200

    Added react router for location selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Forecasts } from "@/features/forecasts";
 import { useState } from "react";
-import cities from "./assets/cities.json";
 import { LocationProvider } from "./contexts/geoid";
 import { CurrentWeather } from "./features/current-weather";
 import { useReloadOnResume } from "./hooks/useReloadOnResume";
@@ -13,13 +12,6 @@ export function App() {
 
   return (
     <LocationProvider value={{ geoId, location, setGeoId, setLocation }}>
-      <select>
-        {cities.map((city, i) => (
-          <option key={i} value={city}>
-            {city}
-          </option>
-        ))}
-      </select>
       <CurrentWeather />
       <Forecasts />
     </LocationProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App.tsx";
 import "./global.css";
 
@@ -11,12 +12,18 @@ if (!rootElement) {
 }
 
 const root = createRoot(rootElement);
+const router = createBrowserRouter([
+  {
+    path: "/weather",
+    Component: App,
+  },
+]);
 const queryClient = new QueryClient();
 
 root.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
 );

c7497c8b09b183ddda3930ef73c913fa4590c798
Juha Laurikainen <jmlaurikainen@gmail.com>
2025-04-06 19:47:14 +0300

    Tuned border colors. Switched button icon to left

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -11,10 +11,10 @@ export function Button(props: Props) {
     <button
       type="button"
       {...rest}
-      className={`flex cursor-pointer items-center justify-center gap-2 rounded-lg border-1 border-slate-600 ${props.children == null ? "px-2" : "px-4"} py-2 outline-0 hover:border-slate-400 focus-visible:border-slate-400 active:border-slate-500`}
+      className={`border-outer-space flex cursor-pointer items-center justify-center gap-2 rounded-lg border-1 ${props.children == null ? "px-2" : "px-4"} py-2 outline-0 hover:border-slate-400 focus-visible:border-slate-400 active:border-slate-500`}
     >
-      {rest.children}
       {Icon ? <Icon height={24} width={24} className="fill-current" /> : null}
+      {rest.children}
     </button>
   );
 }
--- src/components/atoms/menu/index.tsx
+++ src/components/atoms/menu/index.tsx
@@ -6,7 +6,7 @@ export function Menu(props: Props) {
   return (
     <dialog
       {...props}
-      className="open:animate-in bg-gunmetal fixed mx-auto my-auto max-w-11/12 flex-col overflow-hidden rounded-xl border-1 border-slate-600 text-white shadow-lg outline-0 backdrop:bg-neutral-950 backdrop:opacity-50"
+      className="open:animate-in bg-gunmetal border-outer-space fixed mx-auto my-auto max-w-11/12 flex-col overflow-hidden rounded-xl border-1 text-white shadow-lg outline-0 backdrop:bg-neutral-950 backdrop:opacity-50"
     />
   );
 }

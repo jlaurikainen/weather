a537daa65e86c71b8604e4f81ddbb5bfbfa12a53
Juha Laurikainen <jmlaurikainen@gmail.com>
2025-04-13 18:34:42 +0300

    Used multipoint coverage query for forecast data. Added wind direction and speed values

--- src/features/forecasts/index.tsx
+++ src/features/forecasts/index.tsx
@@ -6,14 +6,10 @@ export function Forecasts() {
   const [params] = useSearchParams();
   const { data } = useForecasts(params.get("q"));
 
-  const forecasts =
-    data?.members.filter((value) => value.type === "Temperature")[0].values ??
-    [];
-
   return (
     <div className="flex max-w-full self-center overflow-x-auto p-2 opacity-75">
-      {forecasts.map((forecast) => (
-        <Forecast key={forecast.time.toString()} forecast={forecast} />
+      {Array.from(data?.entries() ?? []).map(([time, values]) => (
+        <Forecast key={time} time={time} values={values} />
       ))}
     </div>
   );

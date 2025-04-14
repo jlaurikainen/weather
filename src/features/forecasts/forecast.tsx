aed6240d33d83e1952eb772d7a6eb3313c2e905d
Juha Laurikainen <jmlaurikainen@gmail.com>
2025-04-14 06:57:46 +0300

    Added weather symbols for forecast values

--- src/features/forecasts/forecast.tsx
+++ src/features/forecasts/forecast.tsx
@@ -1,10 +1,12 @@
 import { IconArrowDownward } from "@/components/atoms/icons";
 import { formatDecimal } from "@/utils/number";
+import { getWeatherStringAndSymbol } from "@/utils/weather";
 
 type Props = {
   time: string | undefined;
   values: {
     temperature: number;
+    weatherSymbol: number;
     windDirection: number;
     windSpeedMS: number;
   };
@@ -12,12 +14,17 @@ type Props = {
 
 export function Forecast(props: Props) {
   const time = new Date(parseInt(props.time ?? "0") * 1000);
+  const weatherStringAndSymbol = getWeatherStringAndSymbol(
+    props.values.weatherSymbol,
+  );
+  const Icon = weatherStringAndSymbol?.[1];
 
   return (
     <div className="flex w-18 shrink-0 flex-col items-center gap-0.5 font-extralight">
       <p className="text-sm opacity-75">
         {time.toLocaleTimeString("fi", { hour: "numeric", minute: "2-digit" })}
       </p>
+      {Icon ? <Icon fill="currentcolor" height={24} width={24} /> : null}
       <p>{formatDecimal(props.values.temperature)}°C</p>
       <p className="text-sm opacity-75">
         {formatDecimal(props.values.windSpeedMS)}m/s

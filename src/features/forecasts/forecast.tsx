33c0e101899a1d3473c70fdd4333a55f7d686b79
Juha Laurikainen <jmlaurikainen@gmail.com>
2025-04-14 03:53:23 +0300

    Tuned fonts

--- src/features/forecasts/forecast.tsx
+++ src/features/forecasts/forecast.tsx
@@ -15,13 +15,15 @@ export function Forecast(props: Props) {
 
   return (
     <div className="flex w-18 shrink-0 flex-col items-center gap-0.5 font-extralight">
-      <p>
+      <p className="text-sm opacity-75">
         {time.toLocaleTimeString("fi", { hour: "numeric", minute: "2-digit" })}
       </p>
       <p>{formatDecimal(props.values.temperature)}°C</p>
-      <p>{formatDecimal(props.values.windSpeedMS)}m/s</p>
+      <p className="text-sm opacity-75">
+        {formatDecimal(props.values.windSpeedMS)}m/s
+      </p>
       <p style={{ rotate: `${props.values.windDirection}deg` }}>
-        <IconArrowDownward fill="currentcolor" height={32} width={32} />
+        <IconArrowDownward fill="currentcolor" height={24} width={24} />
       </p>
     </div>
   );
